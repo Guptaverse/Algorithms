29be7801d1afef222dd093c6650f5fc510d0e187
Guptaverse <sggreat88@gmail.com>
2022-01-07 17:02:10 +0530

    bubble sort updated1

--- bubbleSort/bubbleSort.cpp
+++ bubbleSort/bubbleSort.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(int *a,int n){
+void bubbleSort(int a[],int n){
+    if(n==1){
+        return;
+    }
+    for(int j=0;j<n;j++){
+        if(a[j]>a[j+1]){
+            swap(a[j],a[j+1]);
+        }
 
+    }
+    bubbleSort(a,n-1);
 }
-int main()
+int main(){
+    int a[] = {5,4,3,1,2};
+    int n=5;
+    bubbleSort(a,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
